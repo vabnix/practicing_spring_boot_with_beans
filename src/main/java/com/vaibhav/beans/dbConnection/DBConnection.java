1d052f00646a733b2ab9721921fd3feb4da57bc6
Vaibhav Srivastava <vabnix@hotmail.com>
2025-02-04 23:33:46 -0600

    by using the properties file

--- src/main/java/com/vaibhav/beans/dbConnection/DBConnection.java
+++ src/main/java/com/vaibhav/beans/dbConnection/DBConnection.java
@@ -2,10 +2,23 @@ package com.vaibhav.beans.dbConnection;
 
 import jakarta.annotation.PostConstruct;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
 @Component
 public class DBConnection {
+
+    @Value("${username}")
+    String username;
+
+    @Value("${password}")
+    String password;
+
+    @Value("${dbUrl}")
+    String dbUrl;
+
+    @Value("${port}")
+    Integer port;
 
     @Autowired(required = false)
     PostgresConnection postgresConnection;
@@ -18,5 +31,10 @@ public class DBConnection {
         System.out.println("DBConnection Init");
         System.out.println("PostgresConnection - "+ postgresConnection);
         System.out.println("NoSqlConnection -"+ noSqlConnection);
+
+        System.out.println("username - "+ username + " " +
+                ", password : "+ password + " " +
+                ", dburl: "+ dbUrl +
+                " portNumber : "+ port);
     }
 }
