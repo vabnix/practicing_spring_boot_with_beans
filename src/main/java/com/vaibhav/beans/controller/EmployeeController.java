fb369ad54e80c457596deaaa3d38afef82e12f4a
Vaibhav Srivastava <vabnix@hotmail.com>
2025-02-04 19:31:28 -0600

    validating the user session

--- src/main/java/com/vaibhav/beans/controller/EmployeeController.java
+++ src/main/java/com/vaibhav/beans/controller/EmployeeController.java
@@ -3,12 +3,16 @@ package com.vaibhav.beans.controller;
 import com.vaibhav.beans.entity.Employee;
 import com.vaibhav.beans.entity.User;
 import jakarta.annotation.PostConstruct;
+import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Scope;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@Scope("prototype")
+@Scope("session")
 public class EmployeeController {
 
     @Autowired
@@ -26,4 +30,18 @@ public class EmployeeController {
         System.out.println("Employee Controller Hashcode - "+ this.hashCode()
                 + " Employee Object Hashcode -- " + employee.hashCode());
     }
+
+    @GetMapping("/employee")
+    public ResponseEntity<String> fetchEmployee(HttpServletRequest request){
+        System.out.println("Fetching the API Request called");
+        return ResponseEntity.status(HttpStatus.OK).body("OK");
+    }
+
+    @GetMapping("/employee/logout")
+    public ResponseEntity<String> logoutEmployee(HttpServletRequest request){
+        System.out.println("User is logged out");
+        request.getSession().invalidate();
+        return ResponseEntity.status(HttpStatus.OK).body("OK");
+    }
+
 }
